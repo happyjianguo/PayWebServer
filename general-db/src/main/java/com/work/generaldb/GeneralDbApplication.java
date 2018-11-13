4eb2c789eeb2da98b519cd30378a62faa281b0b5
whj826069748 <39262392+whj826069748@users.noreply.github.com>
2018-11-13 13:49:21 +0800

    1

---
+++ general-db/src/main/java/com/work/generaldb/GeneralDbApplication.java
@@ -0,0 +1,12 @@
+package com.work.generaldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralDbApplication.class, args);
+	}
+}
