58410a6c57b0e20c98cf7c61470da50ee80f95de
whj826069748 <39262392+whj826069748@users.noreply.github.com>
2018-11-14 16:49:28 +0800

    aop切面

--- service-ali/src/main/java/com/work/serviceali/ServiceAliApplication.java
+++ service-ali/src/main/java/com/work/serviceali/ServiceAliApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.work."})
+@ComponentScan(basePackages = {"com.work.*"})
 public class ServiceAliApplication {
 
 	public static void main(String[] args) {
