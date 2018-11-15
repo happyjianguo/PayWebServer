389346d7011abb23b9482078821bd18a64266c69
whj826069748 <39262392+whj826069748@users.noreply.github.com>
2018-11-15 09:08:16 +0800

    aop切面k

---
+++ redis-db/src/main/java/com/work/redisdb/RedisDbApplication.java
@@ -0,0 +1,12 @@
+package com.work.redisdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisDbApplication.class, args);
+	}
+}
