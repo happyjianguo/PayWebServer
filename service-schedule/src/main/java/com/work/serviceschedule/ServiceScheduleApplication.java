c62df8a65a4d3d2b11ee6e9bd5d14c341e465801
whj826069748 <39262392+whj826069748@users.noreply.github.com>
2018-11-29 16:23:24 +0800

    定时器

--- service-schedule/src/main/java/com/work/serviceschedule/ServiceScheduleApplication.java
+++ service-schedule/src/main/java/com/work/serviceschedule/ServiceScheduleApplication.java
@@ -1,16 +1,16 @@
 package com.work.serviceschedule;
 
-import org.apache.ibatis.session.Configuration;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableEurekaClient
+@EnableFeignClients
 @MapperScan("com.work.generaldb.mapper")
 @ComponentScan(basePackages = {"com.work.*"})
 @SpringBootApplication
